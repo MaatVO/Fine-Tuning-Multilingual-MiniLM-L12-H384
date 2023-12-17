4c4cc04e6d5dee403555ff00770c6ab0fa6651e8
MaatVO <102108918+MaatVO@users.noreply.github.com>
2023-12-17 16:15:08 +0100

    Update

--- main.py
+++ main.py
@@ -1,149 +1,129 @@
-import datasets
-from numba import cuda
 import pandas as pd
 import numpy as np
-from transformers import DataCollatorForSeq2Seq
-from transformers import TFAutoModelForSeq2SeqLM
-from transformers import AutoTokenizer
-from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
-from transformers import create_optimizer
-from datasets import Dataset
-from tqdm import tqdm
-from transformers import get_scheduler
-from torch.optim import AdamW
-import torch.cuda
-from typing import Tuple, Dict
+import os
+from transformers import  (AutoModel,AutoModelForSequenceClassification,
+                           DataCollatorWithPadding,AutoTokenizer,TrainingArguments,Trainer)
+from datasets import Dataset,DatasetDict
+import evaluate
+accuracy = evaluate.load("accuracy")
+import torch
+from sklearn.metrics import accuracy_score
 
 
-# İlk hocanın paperinin yaptığının aynısını dene
-# Sonra hugging face transformers kullan.
-# Sonra sadece q1 değil q2 yi de eklemek gerekebilir.
-# Transfer Learning, Lora, PEFT techniques
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return accuracy.compute(predictions=predictions, references=labels)
 
-def csv_to_df(path: str = None, sample: int = 50) -> pd.DataFrame:
+
+def csv_to_df(path: str = None,question_type:int=1, sample: int = 100) -> pd.DataFrame:
+    """
+
+    :param path: "csv file path
+    :param question_type: for q1 ->1, for q2->2, for q1+q2->3
+    :param sample: 0 is full dataset, anything else is number of rows
+    :return: pandas dataframe
+    """
     df = pd.read_csv(path, encoding="ISO-8859-1")
-    df.loc[df['outcome_class'] == 't', 'outcome_class'] = 'T'
-    df.loc[df['outcome_class'] == 'd', 'outcome_class'] = 'F'
-    df['q1'] = df['q1'].apply(lambda x: x.replace('\n', ''))
-    df = df.rename(columns={'q1': 'sent', 'outcome_class': 'labels'})
-    df = df[['sent', 'labels']]
+
     if sample != 0:
-        return df.iloc[:sample, :]
-    else:
-        return df
+        df = df.sample(n=sample, replace='False')
 
 
-def create_dataset_with_crossval(df: pd.DataFrame = None, seed: int = 42, cv: int = 2) \
-        -> Tuple[Dict[str, pd.DataFrame], Dict[str, pd.DataFrame]]:
-    train = {f'split_{i + 1}': [] for i in range(cv)}
-    test = {f'split_{i + 1}': [] for i in range(cv)}
-    np.random.seed(seed)
+    df.loc[df['outcome_class'] == 't', 'outcome_class'] = 1
+    df.loc[df['outcome_class'] == 'd', 'outcome_class'] = 0
+    if question_type == 1:
+        df['q'] = df['q1'].apply(lambda x: x.replace('\n', ''))
+    elif question_type ==2:
+        df['q'] = df['q2'].apply(lambda x: x.replace('\n', ''))
+    elif question_type == 3:
+        df['q1'] = df['q1'].apply(lambda x: x.replace('\n', ''))
+        df['q2'] = df['q2'].apply(lambda x: x.replace('\n', ''))
+        df['q'] = df['q1'] + df['q2']
 
-    in_arr = np.arange(0, len(df))
-    np.random.shuffle(in_arr)
-    in_arr_r = in_arr.reshape((cv, int(len(df) / cv)))  # reshape data (num cv, total_rows/num cv)
-
-    cv_ind = 0
-    for k in train.keys():  # train.keys() = split_1, split_2 ...
-
-        ind = in_arr_r[cv_ind, :]  # test indices
-        antind = [i for i in in_arr if i not in ind]  # train indices
-
-        intent_train = df[df.index.isin(antind)]
-        intent_test = df[df.index.isin(ind)]
-
-        train[k].append(intent_train)
-        test[k].append(intent_test)
-
-        train[k] = pd.concat(train[k])
-        test[k] = pd.concat(test[k])
-
-        cv_ind += 1
-
-    # train,test: dicts with num cv splits, each split has len(data)*(1-1/cv), len(data)*1/cv
-    return train, test
-
-
-def preprocess_function(examples: datasets.Dataset) -> datasets.Dataset:
-    inputs = [ex for ex in examples["sent"]]
-    targets = [ex for ex in examples["labels"]]
-    model_inputs = tokenizer(
-        inputs, text_target=targets, truncation=True)
+    df = df.rename(columns={'q': 'sent', 'outcome_class': 'label'})
+    df = df[['sent', 'label']]
+    return df
+def tokenize_function(examples: Dataset) -> Dataset:
+    model_inputs = tokenizer(examples["sent"],truncation=True)
     return model_inputs
-
 
 seed = 42
 np.random.seed(seed)
-numcv = 10
-intent_df = csv_to_df('data/sign_events_data_statements.csv')
-train, test = create_dataset_with_crossval(df=intent_df, seed=seed, cv=numcv)
+data_path = 'data/sign_events_data_statements.csv'
+question_type = 3
+data_sample_size = 800
+intent_df = csv_to_df(path=data_path,
+                      question_type=question_type,
+                      sample = data_sample_size)
 
-results = {}
-preds = {}
-trus = {}
-collect_result = {}
-model_size = 'small'
-num_epochs = 3
+dataset = Dataset.from_pandas(intent_df)
+train_testvalid = dataset.train_test_split(test_size= 0.1, shuffle=True)
+valid_test = train_testvalid["test"].train_test_split(test_size=0.5,shuffle=True)
+dataset_splitted = DatasetDict({
+    'train': train_testvalid["train"],
+    'valid': valid_test["train"],
+    'test': valid_test["test"]
+})
 
-for sp in tqdm(train.keys()):
+checkpoint = "microsoft/Multilingual-MiniLM-L12-H384"
+path = os.path.join("./models/pretrained",checkpoint)
+tokenizer = AutoTokenizer.from_pretrained(path)
+model = AutoModel.from_pretrained(path)
+id2label = {0: "F", 1: "T"}
+label2id = {"F": 0, "T": 1}
 
-    data_train = Dataset.from_pandas(train[sp])
-    data_test = Dataset.from_pandas(test[sp])
+model = AutoModelForSequenceClassification.from_pretrained(
+path, num_labels=2, id2label=id2label, label2id=label2id
+                                   )
+tokenized_dataset = dataset_splitted.map(tokenize_function, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-    checkpoint = f"google/flan-t5-{model_size}"
-    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-    model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
+for param in model.bert.encoder.layer[:3].parameters():  # Freeze the first 3 layers of the encoder
+    param.requires_grad = False
 
-    data_train = data_train.map(preprocess_function, batched=True)
-    data_test = data_test.map(preprocess_function, batched=True)
 
-    data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
+num_epochs = 10
+training_args = TrainingArguments(
+    output_dir="my_awesome_model",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=num_epochs,
+    weight_decay=0.01,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    use_cpu= False
+)
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset["train"],
+    eval_dataset=tokenized_dataset["valid"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+#trainer.train()
 
-    training_args = Seq2SeqTrainingArguments(
-        output_dir=f"{model_size}_scenario_intent",
-        evaluation_strategy='epoch',
-        learning_rate=5e-5,
-        per_device_train_batch_size=2,
-        weight_decay=0.01,
-        save_total_limit=3,
-        num_train_epochs=num_epochs,
-        predict_with_generate=True,
-        fp16=False,
-        push_to_hub=False,
-    )
-    trainer = Seq2SeqTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=data_train,
-        eval_dataset=data_test,
-        tokenizer=tokenizer,
-        data_collator=data_collator
-    )
+# Prediction
+predictions = []
+labels = []
+for example in tokenized_dataset["test"]:
+    # Tokenize input
+    inputs = tokenizer(example['sent'], return_tensors='pt')
 
-    trainer.train()
+    # Run inference
+    outputs = model(**inputs)
 
-    pred = []
-    for i in test[sp]['sent']:
-        toki = tokenizer(i, return_tensors="pt").input_ids   # return token id's as pytorch tensor
-        h = model.generate(toki, return_dict_in_generate=True, output_scores=True)
-        decoded_preds = tokenizer.batch_decode(h.sequences, skip_special_tokens=True)
-        pred.append(decoded_preds[0])
+    # Get predictions and labels
+    prediction = torch.argmax(outputs.logits).item()
+    label = example['label']  # Replace 'label' with the actual key in your dataset
 
-    pred = np.array(pred)
+    predictions.append(prediction)
+    labels.append(label)
 
-    preds[sp] = pred
-    trus[sp] = np.array(test[sp]['labels'])
-
-    collect_result[sp] = pd.DataFrame.from_dict(
-        {"prediction": pred,
-         'labels': np.array(test[sp]['labels'])})
-
-    collect_result[sp]['corr'] = collect_result[sp]['prediction'] == collect_result[sp]['labels']
-    results[sp] = [(pred == trus[sp]).mean()]
-    print("collect results:",collect_result[sp].head(20))
-    del model
-    del trainer
-
-results = pd.DataFrame.from_dict(results)
-print('Average accuracy: ', results.mean(axis=1))
-print('Max accuracy: ', results.max(axis=1))
+# Calculate accuracy
+accuracy = accuracy_score(labels, predictions)
+print(f'Accuracy: {accuracy * 100:.2f}%')
